9cef66b1fba57a53e0b71fe1a44d156a290ced7c
samuelfsantos <samu_192.sa@hotmail.com>
2023-04-06 23:41:00 -0300

    Update ProdutoController.cs
    Alteração teste

--- Puc.Mercadinho/Puc.Mercadinho.Api/Controllers/ProdutoController.cs
+++ Puc.Mercadinho/Puc.Mercadinho.Api/Controllers/ProdutoController.cs
@@ -48,7 +48,7 @@ namespace Puc.Mercadinho.Api.Controllers
             try
             {
 
-                var result = "Esta API esta funcionando!";
+                var result = "Esta API esta funcionando! Testando sexta-feira santa!";
 
                 return Ok(result);
             }
